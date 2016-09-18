ee61e3d1db153fac88a5bd9feafa220c68459de3
yolorusher <yupiumka@yandex.ru>
2016-09-18 08:57:46 -0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace RecController {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(21, 98);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(173, 78);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "ЗАПУСКАЕМ ФСБ";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "            Микрофон";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(126, 56);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(54, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(68, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Клавиша";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(21, 25);
+            this.comboBox1.MaxDropDownItems = 10;
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(173, 21);
+            this.comboBox1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(216, 194);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "YoloRec";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NAudio.CoreAudioApi;
+
+namespace RecController {
+    public partial class Form1 : Form {
+        bool isWorking = false;
+        bool setKey = false;
+        bool coolDown = false;
+        string microName = "";
+        int keyCode = -1;
+        MMDevice micro;
+        MMDevice[] allDevices;
+        Hook _hook;
+
+        public Form1() {
+            InitializeComponent();
+            KeyPreview = true;
+            button1.Click += button1_Click;
+            textBox2.Click += TextBox2_Click;
+            comboBox1.TextChanged += ComboBox1_TextChanged;
+            KeyDown += Form1_KeyDown;
+            FormClosed += Form1_FormClosed;
+
+            var devices = new MMDeviceEnumerator().EnumerateAudioEndPoints(DataFlow.Capture, DeviceState.Active);
+            allDevices = new MMDevice[devices.Count];
+            for (int i = 0; i < devices.Count; i++) {
+                allDevices[i] = devices[i];
+                comboBox1.Items.Add(devices[i].DeviceFriendlyName);
+            }
+
+            if (!File.Exists($@"{Environment.CurrentDirectory}\config.xml"))
+                using (var stream = new FileStream($@"{Environment.CurrentDirectory}\config.xml", FileMode.OpenOrCreate)) { }
+            var str = File.ReadAllLines($@"{Environment.CurrentDirectory}\config.xml");
+            
+            if (str.Length >= 3) {
+                microName = str[0];
+
+                int res;
+                if (int.TryParse(str[1], out res))
+                    keyCode = res;
+
+                for (int i = 0; i < allDevices.Length; i++) {
+                    if (allDevices[i].DeviceFriendlyName == microName)
+                        comboBox1.SelectedItem = microName;
+                }
+                textBox2.Text = str[2];
+            }
+        }
+
+        private void ComboBox1_TextChanged(object sender, EventArgs e) {
+            microName = comboBox1.SelectedItem.ToString();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e) {
+            if (setKey) {
+                keyCode = (int)e.KeyCode;
+                textBox2.Text = e.KeyCode.ToString();
+                setKey = false;
+            }
+        }
+        private void TextBox2_Click(object sender, EventArgs e) {
+            if(!isWorking)
+                setKey = true;
+        }
+        private void button1_Click(object sender, EventArgs e) {
+            if (!isWorking) {
+                for (int i = 0; i < allDevices.Length; i++) {
+                    if (allDevices[i].DeviceFriendlyName == microName)
+                        micro = allDevices[i];
+                }
+
+                if (micro != null) {
+                    _hook = new Hook(keyCode);
+                    _hook.KeyPressed += _hook_KeyPressed;
+                    _hook.SetHook();
+                    label1.Text = "            Микрофон";
+                } else {
+                    label1.Text = "МИКРОФОН НЕ НАЙДЕН";
+                }
+                button1.Text = micro.AudioEndpointVolume.Mute ? "ВЫКЛЮЧЕН" : "ВКЛЮЧЕН";
+            } else {
+                _hook?.Dispose();
+                button1.Text = "ЗАПУСКЕМ ФСБ";
+            }
+            isWorking = !isWorking;
+        }
+
+        private void _hook_KeyPressed(object sender, KeyPressEventArgs e) {
+            if (!coolDown) {
+                coolDown = true;
+                micro.AudioEndpointVolume.Mute = !micro.AudioEndpointVolume.Mute;
+                Task.Factory.StartNew(() => {
+                    Thread.Sleep(100);
+                    coolDown = false;
+                });
+            }
+            button1.Text = micro.AudioEndpointVolume.Mute ? "ВЫКЛЮЧЕН" : "ВКЛЮЧЕН";
+        }
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e) {
+            File.WriteAllLines($@"{Environment.CurrentDirectory}\config.xml", new[] { microName, keyCode.ToString(), textBox2.Text });
+        }
+    }
+
+
+    public class Hook : IDisposable {
+        #region Declare WinAPI functions
+        [DllImport("kernel32.dll")]
+        private static extern IntPtr LoadLibrary(string lpFileName);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr SetWindowsHookEx(int idHook, KeyboardHookProc callback, IntPtr hInstance, uint threadId);
+        [DllImport("user32.dll")]
+        private static extern IntPtr CallNextHookEx(IntPtr idHook, int nCode, int wParam, IntPtr lParam);
+        [DllImport("user32.dll")]
+        private static extern bool UnhookWindowsHookEx(IntPtr hInstance);
+        #endregion
+        #region Constants
+        private const int WH_KEYBOARD_LL = 13;
+        private const int WH_KEYDOWN = 0x0100;
+        #endregion
+
+        // код клавиши на которую ставим хук
+        private int _key;
+        public event KeyPressEventHandler KeyPressed;
+
+        private delegate IntPtr KeyboardHookProc(int code, IntPtr wParam, IntPtr lParam);
+        private KeyboardHookProc _proc;
+        private IntPtr _hHook = IntPtr.Zero;
+
+        public Hook(int keyCode) {
+            _key = keyCode;
+            _proc = HookProc;
+        }
+
+        public void SetHook() {
+            var hInstance = LoadLibrary("User32");
+            _hHook = SetWindowsHookEx(WH_KEYBOARD_LL, _proc, hInstance, 0);
+        }
+
+        public void Dispose() {
+            UnHook();
+        }
+
+        public void UnHook() {
+            UnhookWindowsHookEx(_hHook);
+        }
+
+        private IntPtr HookProc(int code, IntPtr wParam, IntPtr lParam) {
+            if ((code >= 0 && wParam == (IntPtr)WH_KEYDOWN) && Marshal.ReadInt32(lParam) == _key) {
+
+                // бросаем событие
+                if (KeyPressed != null) {
+                    KeyPressed(this, new KeyPressEventArgs(Convert.ToChar(code)));
+                }
+            }
+
+            // пробрасываем хук дальше
+            return CallNextHookEx(_hHook, code, (int)wParam, lParam);
+        }
+    }
+}
